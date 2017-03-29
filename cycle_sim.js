caa461e4cbfa50a91d03d884a4fff1fda0dd271e
asteriskman7 <asteriskman@gmail.com>
2017-03-29 08:39:07 -0400

    Added GPLv3 LICENSE.txt and updated files with appropriate license info

--- cycle_sim.js
+++ cycle_sim.js
@@ -1,8 +1,20 @@
 /* dld_simulator.js
-   Author: asterisk_man
-   Date: 16 December 2013
    Description: digital logic cycle simulator
-                This file copyright asterisk_man 2014. All rights reserved
+
+    Copyright (C) 2014 asterisk_man
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
    Use:
      Set cycle_sim.logElement to the textarea to be used as a log window.
